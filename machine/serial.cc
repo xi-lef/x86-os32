fdb7e5ace4a247ec10216232461628c795949558
Felix Windsheimer <felix.windsheimer@fau.de>
2018-11-22 12:10:28 +0100

    helper functions for serial implemented

--- machine/serial.cc
+++ machine/serial.cc
@@ -5,52 +5,59 @@ Serial::Serial(Serial::comPort port, Serial::baudRate baudrate, Serial::dataBits
     port(port), io_port_buffer(port), io_port_lcr(port + lcr), io_port_ier(port + ier), io_port_lsr(port + lsr) {
     /// baudrate
     // set DLAB bit
-    io_port_lcr.outb(1 << 6);
+    //io_port_lcr.outb(1 << 7);
+    writeReg(lcr, 1 << 7);
 
     // write lowbyte to transmitting buffer (DLAB low)
-    io_port_buffer.outb(baudrate & 0xff);
+    //io_port_buffer.outb(baudrate & 0xff);
+    writeReg(tbr, baudrate & 0xff);
     // write highbyte to ier (DLAB high)
-    io_port_ier.outb((baudrate >> 8) & 0xff);
+    //io_port_ier.outb((baudrate >> 8) & 0xff);
+    writeReg(ier, (baudrate >> 8) & 0xff);
 
     // unset DLAB bit
-    io_port_lcr.outb(0 << 7);
+    //io_port_lcr.outb(0 << 7);
+    writeReg(lcr, 0 << 7);
 
     /// databits + stopbits + parity
-    io_port_lcr.outb(databits | stopbits | parity);
+    //io_port_lcr.outb(databits | stopbits | parity);
+    writeReg(lcr, databits | stopbits | parity);
 
     // prepare ier for reading
-    io_port_ier.outb(0x00);
+    //io_port_ier.outb(0x00);
+    writeReg(ier, 0x00);
     //DBG << "serial: finished init" << endl;
 }
 
 void Serial::writeReg(Serial::regIndex reg, char out){
-	(void) reg;
-	(void) out;
+    IO_Port p(port + reg);
+    p.outb(out);
 }
 
 char Serial::readReg(Serial::regIndex reg){
-	(void) reg;
-	return '\0';
+    IO_Port p(port + reg);
+	return p.inb();
 }
 
 int Serial::write(char out, bool blocking){
-    if (!blocking && !(io_port_lsr.inb() & (1 << 5))) {
+    if (!blocking && !(readReg(lsr) & (1 << 5))) {
         DBG << "serial: bad write" << endl;
         return -1;
     }
-    while (!(io_port_lsr.inb() & (1 << 5)));
+    while (!(readReg(lsr) & (1 << 5)));
 
-    io_port_buffer.outb(out);
+    //io_port_buffer.outb(out);
+    writeReg(tbr, out);
 
 	return out;
 }
 
 int Serial::read(bool blocking){
-    if (!blocking && !(io_port_lsr.inb() & 1)) {
+    if (!blocking && !(readReg(lsr) & 1)) {
         DBG << "serial: bad read" << endl;
         return -1;
     }
-    while (!(io_port_lsr.inb() & 1));
+    while (!(readReg(lsr) & 1));
 
-    return io_port_buffer.inb();
+    return readReg(rbr);
 }
